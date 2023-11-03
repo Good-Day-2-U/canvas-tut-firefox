e7dd214dfc5b8f28da94e583d83a8386bcd6833b
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-11-03 15:03:25 -0700

    Create boilerplate links and files

---
+++ main.js
@@ -0,0 +1,7 @@
+function draw() {
+  const canvas = document.getElementById("tutorial");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+  }
+}
+window.addEventListener("load", draw);
